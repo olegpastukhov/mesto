1ba8ce6e6d1a32446fdf0e7e01ba156672267bef
Oleg Pastukhov <Admin@S-1530-036.vao.obr.mos.ru>
2022-07-07 10:37:39 +0300

    Проект 4 попытка 2 после ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,13 @@ let formElement = document.querySelector('.form');
 let popupOpenButtonElement = document.querySelector('.profile__edit-button');
 let nameInput = formElement.querySelector('#name');
 let jobInput = formElement.querySelector('#job');
+let profileTitleElement = document.querySelector('.profile__title');
+let profileDescriptionElement = document.querySelector('.profile__description');
 
 // Функции
 const openPopup = function () {
+  nameInput.value = profileTitleElement.textContent
+  jobInput.value = profileDescriptionElement.textContent
   popupElement.classList.add('popup_opened');
 }
 
@@ -17,16 +21,11 @@ const closePopup = function () {
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  popupElement.classList.remove('popup_opened');
-  // Получите значение полей jobInput и nameInput из свойства value
   let name = nameInput.value;
   let job = jobInput.value;
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let profileTitleElement = document.querySelector('.profile__title');
-  let profileDescriptionElement = document.querySelector('.profile__description');
-  // Вставьте новые значения с помощью textContent
   profileTitleElement.textContent = name;
   profileDescriptionElement.textContent = job;
+  closePopup();
 }
 
 // События
